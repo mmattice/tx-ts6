f7bebf0363584c5e745cf96f1e565e512f7e9425
Mike Mattice <mike@milliways.cx>
2010-06-20 18:43:48 -0500

    Add multiclient module loading to example-ext.py (extended version of example.pl)

---
+++ clients/idoru.py
@@ -0,0 +1,40 @@
+from ts6.client import Client
+
+class Idoru(Client):
+    def userJoined(self, client, channel):
+        Client.userJoined(self, client, channel)
+        print '%s: saw join %s %s' % (self.nick, client, channel)
+
+    def joined(self, channel):
+        Client.joined(self, channel)
+        print '%s: joined %s' % (self.nick, channel)
+
+    def userLeft(self, client, channel, message):
+        Client.userLeft(self, client, channel, message)
+        print '%s: saw part %s %s "%s"' % (self.nick, client, channel, message)
+
+    def left(self, channel):
+        Client.left(self, channel)
+        print '%s: left %s' % (self.nick, channel)
+
+    def userQuit(self, client, message):
+        Client.userQuit(self, client, message)
+        print '%s: saw quit %s "%s"' % (self.nick, client, message)
+
+    def privmsg(self, client, target, message):
+        print '%s: saw privmsg %s->%s "%s"' % (self.nick, client, target, message)
+
+    def noticed(self, client, target, message):
+        print '%s:  saw notice %s->%s "%s"' % (self.nick, client, target, message)
+
+    def kickedFrom(self, channel, kicker, message):
+        print '%s was kicked from %s by %s "%s"' % (self, channel, kicker, message)
+        self.join(channel)
+
+    def userKicked(self, kickee, channel, kicker, message):
+        print '%s: saw %s kick %s from %s "%s"' % (self, kicker, kickee, channel, message)
+
+    def signedOn(self):
+        print '%s: signedOn' % (self.nick)
+        self.join('#test')
+
---
+++ example-ext.py
@@ -0,0 +1,73 @@
+from ts6.client import Client
+from ts6.ircd import IrcdFactory, IrcdConn
+from ts6.server import Server
+
+import os
+from fnmatch import filter as fnfilter
+
+class TestIrcdConn(IrcdConn):
+    password = 'acceptpw'
+    def sendLine(self, line):
+        IrcdConn.sendLine(self,line)
+        print '-> %s' % line
+
+    def lineReceived(self, line):
+        print '<- %s' % line
+        IrcdConn.lineReceived(self, line)
+
+    def newClient(self, client):
+        print 'twisted-seven: client %s identified as %s' % (client, client.login)
+
+    def loginClient(self, client):
+        print 'twisted-seven: login %s %s' % (client.nick, client.login)
+
+    def burstStart(self):
+        print 'twisted-seven: burst starting'
+        IrcdConn.burstStart(self)
+
+    def burstEnd(self):
+        print 'twisted-seven: burst over'
+
+def dyn_load(loadpath):
+    try:
+        chunks = loadpath.split('.')
+        module = __import__('.'.join(chunks[0:-1]), fromlist=[chunks[-1]])
+        return getattr(module,chunks[-1])
+    except (ImportError, AttributeError):
+        print('Cannot load',loadpath)
+
+def get_clients():
+    cmps = []
+    modules = fnfilter(os.listdir('clients'), '*.py')
+    modules.remove('__init__.py')
+    cmps.extend(map(lambda x: os.path.splitext(x)[0], modules))
+    return cmps
+
+def load_client(name):
+    return dyn_load('clients.%s.%s' % (name, name.capitalize()))
+    
+class TestIrcdFactory(IrcdFactory):
+    protocol = TestIrcdConn
+
+    def __init__(self):
+        self.state.sid = '90B'
+        self.state.servername = 'ts6.grixis.local'
+        self.me = Server(self.state.sid, self.state.servername, self.state.serverdesc)
+        clist = get_clients()
+        classlist = map(load_client, clist)
+        cdict = dict(zip(clist, classlist))
+        print cdict
+        self.clients = [ cl(self, self.me, nick) for nick, cl in cdict.iteritems() ]
+        for c in self.clients:
+            self.state.addClient(c)
+            c.connectionMade()
+
+    def clientConnectionLost(self, connector, reason):
+        print 'connection lost - %s' % (reason,)
+        self.state.cleanNonLocal()
+        connector.connect()
+
+
+from twisted.internet import reactor
+reactor.connectTCP('localhost', 5000, TestIrcdFactory())
+reactor.run()
