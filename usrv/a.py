aed6df3689de0ee2dc8c0c833611ebc10036cb2e
Elly Jones <elly@leptoquark.net>
2010-06-21 22:09:51 -0400

    usrv: finish the A service.

--- usrv/a.py
+++ usrv/a.py
@@ -1,5 +1,6 @@
 from usrv.service import Service
 import re
+import time
 
 authserv = None
 
@@ -18,6 +19,27 @@ class A(Service):
     def getacct(self, name):
         return self.accts.get(name.lower(), None)
 
+    def hasflag(self, src, flag):
+        a = self.getacct(src.login)
+        if not a:
+            return False
+        return flag in a['flags']
+
+    def parseflags(self, flags):
+        set = True
+        add = []
+        remove = []
+        for f in flags:
+            if f == '+':
+                set = True
+            elif f == '-':
+                set = False
+            elif set:
+                add.append(f)
+            else:
+                remove.append(f)
+        return (add, remove)
+
     # REGISTER <account> <password>
     def cmd_register(self, src, target, args):
         ap = args.split(' ')
@@ -32,9 +54,16 @@ class A(Service):
         if not self.isokname(aname):
             self.reply(src, 'Account name %s is invalid.' % aname)
             return
-        self.accts[aname] = { 'pwd': pwd, 'flags': [] }
+        self.accts[aname] = {
+                                'pwd': pwd,
+                                'flags': '',
+                                'reg': int(time.time())
+                            }
         self.conn.login(src, aname)
         self.reply(src, 'Account %s registered.' % aname)
+        if len(self.accts) == 1:
+            self.accts[aname]['flags'] = 'ao'
+            self.reply(src, 'First account marked as administrator.')
 
     # LOGIN <account> <password>
     def cmd_login(self, src, target, args):
@@ -52,3 +81,66 @@ class A(Service):
             return
         self.conn.login(src, aname)
         self.reply(src, 'You are now logged in as %s.' % aname)
+
+    # INFO <account>
+    def cmd_info(self, src, target, args):
+        ap = args.split(' ')
+        if len(ap) != 1:
+            self.reply(src, 'Syntax: INFO <account>')
+            return
+        ag = self.getacct(ap[0])
+        if not ag:
+            self.reply(src, 'No account named %s.' % ap[0])
+            return
+        if ag['flags']:
+            fl = ag['flags']
+        else:
+            fl = '<none>'
+        self.reply(src, 'Account %s: flags %s, registered %ld' % (ap[0], fl, ag['reg']))
+
+    # LOGOUT
+    def cmd_logout(self, src, target, args):
+        if args:
+            self.reply(src, 'Syntax: LOGOUT')
+            return
+        if not src.login:
+            self.reply(src, 'You are not logged in.')
+            return
+        self.conn.logout(src)
+        self.reply(src, 'You are now logged out.')
+
+    # DROP
+    def cmd_drop(self, src, target, args):
+        if args:
+            self.reply(src, 'Syntax: DROP')
+            return
+        if not src.login:
+            self.reply(src, 'You are not logged in.')
+            return
+        del self.accts[src.login.lower()]
+        self.conn.logout(src)
+        self.reply(src, 'Account deleted.')
+
+    # FLAGS <account> <flags>
+    def cmd_flags(self, src, target, args):
+        ap = args.split(' ')
+        if len(ap) != 2:
+            self.reply(src, 'Syntax: FLAGS <account> <flags>')
+            return
+        if not self.hasflag(src, 'a'):
+            self.reply(src, 'Access denied.')
+            return
+        (acct, flags) = ap
+        tu = self.getacct(acct)
+        if not tu:
+            self.reply(src, 'No account named %s.' % acct)
+            return
+        (add, remove) = self.parseflags(flags)
+        flz = list(tu['flags'])
+        for f in add:
+            flz.append(f)
+        for f in remove:
+            flz.remove(f)
+        tu['flags'] = ''.join(flz)
+        self.reply(src, 'Flags for %s are now %s.' % (acct, tu['flags']))
+
